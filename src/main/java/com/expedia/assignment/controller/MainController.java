fd0fb10f54b48fcdb43dd3ba7a73d2cad10f084a
Samiz <mohd90s@gmail.com>
2018-03-12 21:54:25 +0200

    commit

--- src/main/java/com/expedia/assignment/controller/MainController.java
+++ src/main/java/com/expedia/assignment/controller/MainController.java
@@ -1,19 +1,17 @@
 package com.expedia.assignment.controller;
 
-import com.expedia.assignment.CreateRequest;
-import com.expedia.assignment.RequestParams;
 import com.expedia.assignment.filter.RequestFilter;
 import com.expedia.assignment.filterparameter.RequestFilterParam;
-import com.expedia.assignment.model.Hotel;
 import com.expedia.assignment.model.JSONRoot;
 import com.expedia.assignment.parser.ParseJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
@@ -24,7 +22,7 @@ public class MainController {
     }
 
     @ExceptionHandler(RuntimeException.class)
-    @RequestMapping(value = "/welcomePage", method = RequestMethod.POST)
+    @PostMapping("/postPage")
     public String resultPost(@ModelAttribute("requestModel") RequestPageModel requestPage, Model model) {
         model.addAttribute("filter", requestPage);
         ArrayList<String> params = RequestFilterParam.params(requestPage);
